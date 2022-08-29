28bcc0576b1abf1e3da44457407fbd7f486a88ba
n49o7 <n49o7@free.fr>
2022-08-29 22:28:45 +0200

    Implement functions and methods
    Statement of intent using pandas as backend. The following functions are also present as methods of a class Data to allow chaining.
    
    s: select
    f: filter
    j: join
    a: append
    e: extend
    m: map
    r: reduce
    p: pivot
    u: unpivot

--- e.py
+++ e.py
@@ -1,5 +1,76 @@
+# TODO: avoid depending on DataFrames
 
+import pandas as pd
 
-class Array:
-	def __init__(self, object):
-		self.values = 
+def s(data, selection):
+	# Select
+	return data[selection]
+
+def f(data, condition):
+	# Filter
+	return data[condition]
+
+def j(data, left, right, other, operation):
+	# Join
+	o = {
+		'o': 'outer',
+		'i': 'inner',
+		'l': 'left',
+		'r': 'right',
+		'c': 'cross'
+		}
+	return pd.merge(
+		left=data,
+		right=other,
+		left_on=left,
+		right_on=right,
+		how=o[operation]
+		)
+
+def a(data, other):
+	# Append
+	return pd.concat(
+		data,
+		other,
+		ignore_index=True
+		)
+
+def e(data, name, other):
+	# Extend
+	return data.assign(name=other)
+
+def m(data, name, function):
+	# Map
+	data[name] = data.apply(function, axis='columns')
+	return data
+
+def r(data, function, keep):
+	# Reduce
+	return data.groupby(keep).agg(function).reset_index()
+
+def p(data, keep, pivot, value):
+	# Pivot
+	return data.pivot(
+		index=keep,
+		columns=pivot,
+		values=value
+		)
+
+def u(data, keep, variable, values):
+	# Unpivot
+	return data.melt(
+		id_vars=keep,
+		var_name=variable,
+		value_name=values
+		)
+
+_fs = [(name, thing) for (name, thing) in locals().items() if callable(thing)]
+
+class Data(pd.DataFrame):
+	pass
+
+for _n, _t in _fs:
+    setattr(Data, _n, _t)
+
+def read(filepath, **kwargs):
+	return pd.read_csv(filepath, **kwargs)
